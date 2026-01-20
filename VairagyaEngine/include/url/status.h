0eb49a034afa87ed4549ebeb18174101ac505328
BAPPAYNE <sahilkatrodiyalwt@gmail.com>
2026-01-20 23:18:01 +0530

    feat: Implement core web crawler components including URL processing, frontier, and engine orchestration with basic runtime control.

--- VairagyaEngine/include/url/status.h
+++ VairagyaEngine/include/url/status.h
@@ -1,8 +1,11 @@
-#pragma once
+#ifndef STATUS_H
+#define STATUS_H
 
 enum class URLStatus {
-    INVALID,
-    RELATIVE,
-    DISALLOWED,
-    ACCEPTED
+    INVALID_URL,
+    RELATIVE_URL,
+    DISALLOWED_URL,
+    ACCEPTED_URL
 };
+
+#endif // STATUS_H
