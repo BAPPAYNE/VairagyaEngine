30e4f139b0271e7a97a4eb6b39a033a5ff0a4efe
BAPPAYNE <sahilkatrodiyalwt@gmail.com>
2026-02-01 02:58:51 +0530

    feat: Add configuration variables for argparse and logging options

---
+++ VairagyaEngine/src/utils/config.cpp
@@ -0,0 +1,9 @@
+#include "utils/config.h"
+
+using namespace std;
+
+string list_path;          // Path to the list of URLs
+bool crawl_links = false;        // Enable link extraction and recursive crawling
+bool verbose_logging = false;    // Enable verbose logging
+string json_output_path;       // JSON output file name
+string txt_output_path;        // TXT output file name
