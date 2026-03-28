629e99c233cf2d0f3f72c88f38b1787da5cbbad9
BAPPAYNE <sahilkatrodiyalwt@gmail.com>
2026-03-28 22:03:47 +0530

    feat: implement incremental crawling support and add HTTP/HTTPS fetcher utility

---
+++ VairagyaEngine/include/utils/hash.h
@@ -0,0 +1,11 @@
+#ifndef HASH_H
+#define HASH_H
+
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+const basic_string<char> sha256(const string& input);
+
+#endif // HASH_H
---
+++ VairagyaEngine/src/utils/hash.cpp
@@ -0,0 +1,19 @@
+#include "utils/hash.h"
+
+#include <openssl/sha.h>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+// Generate SHA256 hash value from input string
+const basic_string<char> sha256(const string& input) {
+	unsigned char hash[SHA256_DIGEST_LENGTH];
+	SHA256(reinterpret_cast<const unsigned char*>(input.c_str()), input.size(), hash);
+	stringstream ss;
+	for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
+		ss << hex << setw(2) << setfill('0') << static_cast<int>(hash[i]);
+	}
+	return ss.str();
+}
