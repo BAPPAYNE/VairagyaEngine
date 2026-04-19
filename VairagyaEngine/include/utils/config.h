ae0f7ac9add7960f6eee2d32b157552da8c91dc9
BAPPAYNE <sahilkatrodiyalwt@gmail.com>
2026-04-20 01:41:14 +0530

    Added ignore_robots data variable

--- VairagyaEngine/include/utils/config.h
+++ VairagyaEngine/include/utils/config.h
@@ -14,6 +14,7 @@ extern bool verbose_logging;    // Enable verbose logging
 extern string json_output_path;       // JSON output file name
 extern string txt_output_path;        // TXT output file name
 extern bool same_domain;              // Restrict crawling to same domain only
+extern bool ignore_robots;            // Ignore robots.txt rules
 extern unordered_set<string> allowed_domains;  // Set of allowed domains for crawling
 
 #endif // CONFIG_H
--- VairagyaEngine/src/utils/config.cpp
+++ VairagyaEngine/src/utils/config.cpp
@@ -8,4 +8,5 @@ bool verbose_logging = false;    // Enable verbose logging
 string json_output_path;       // JSON output file name
 string txt_output_path;        // TXT output file name
 bool same_domain = false;        // Restrict crawling to same domain only
+bool ignore_robots = false;       // Ignore robots.txt rules
 unordered_set<string> allowed_domains;  // Set of allowed domains for crawling
